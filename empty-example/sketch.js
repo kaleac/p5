88cce851db94e387aaca74eb2448794893f54e5d
Kalea Chu <kaleachu@berkeley.edu>
2021-02-07 14:56:24 -0800

    made edits

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
   // put setup code here
+  createCanvas(400, 400);
+  let green = '#210042';
+  let color = '#188812'
+  let value = green;
 }
 
 function draw() {
   // put drawing code here
+  background(220);
+  if (mouseClicked) {
+    fill(value);
+    triangle(30, 30, 58, 20, 86, 75);
+  }
 }
